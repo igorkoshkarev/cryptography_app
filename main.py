b7b693e1caf532b8466e3293083abe0f329fe692
igorkoshkarev <igor.koshkarev.04@gmail.com>
2025-02-22 16:33:15 +0700

    create main window class

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-# This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMainWindow, QComboBox, QVBoxLayout, QWidget, QPushButton, QSizePolicy
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Шифры")
+        self.setFixedSize(300, 150)
+
+        layout = QVBoxLayout()
+
+        ciphers = QComboBox()
+        ciphers.addItems(['Шифр Адбаша', 'Шифр Цезаря', 'Шифр Решилье'])
+
+        encode_button = QPushButton()
+        decode_button = QPushButton()
+        encode_button.setText("Зашифровать")
+        decode_button.setText('Расшифровать')
+
+        layout.addWidget(ciphers)
+        layout.addWidget(encode_button)
+        layout.addWidget(decode_button)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+
+        self.setCentralWidget(central_widget)
+
+
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    # ...
+    m = MainWindow()
+    m.show()
     sys.exit(app.exec())
