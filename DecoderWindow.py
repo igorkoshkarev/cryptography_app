6f8f9c9f2e2607ab53ab1e7b359f4d2b4ea9a367
igorkoshkarev <igor.koshkarev.04@gmail.com>
2025-02-22 18:27:31 +0700

    create Atbash Decoder Window class

--- DecoderWindow.py
+++ DecoderWindow.py
@@ -23,15 +23,44 @@ class DecoderWindow(QWidget):
 
         l2 = QLabel()
         l2.setText("Расшифрованное сообщение: ")
-        self.encoded = QLabel()
+        self.decoded = QLabel()
 
         self.button = QPushButton()
         self.button.setText("Расшифровать")
 
+        self.button.clicked.connect(self.decrypt)
+
         layout.addWidget(l1)
+        layout.
         layout.addWidget(self.message)
         layout.addWidget(l2)
-        layout.addWidget(self.encoded)
+        layout.addWidget(self.decoded)
         layout.addWidget(self.button)
 
         self.setLayout(layout)
+
+    def decrypt(self):
+        print("decrypt")
+
+
+class AtbashDecoderWindow(DecoderWindow):
+
+    ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+    ALPHABET_BIG = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    N = 26
+
+    def decrypt(self):
+        text = self.message.text()
+        decrypt_text = ""
+        for i in text:
+            if i in self.ALPHABET:
+                ind = self.ALPHABET.index(i)
+
+                decrypt_text += self.ALPHABET[self.N-(ind+1)]
+            elif i in self.ALPHABET_BIG:
+                ind = self.ALPHABET_BIG.index(i)
+                decrypt_text += self.ALPHABET_BIG[self.N-(ind+1)]
+            else:
+                decrypt_text += i
+            self.decoded.setText(decrypt_text)
+
